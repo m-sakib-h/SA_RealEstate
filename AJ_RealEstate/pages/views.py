2aa064b8d2bb00e97e30441838e16a6bec5a3681
Mohammad Sakib Hossain <sakib.swe.diu@gmail.com>
2020-10-18 01:51:19 +0600

    Model View from Backend in Index

--- AJ_RealEstate/pages/views.py
+++ AJ_RealEstate/pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from listings.models import Listing
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+    latest = Listing.objects.order_by('-list_date')[:3]
+
+    context = {
+        'latest': latest
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
