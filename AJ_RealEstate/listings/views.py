151a79f8ec4b04516d9858dfafa55d9e3e529ff6
Mohammad Sakib Hossain <sakib.swe.diu@gmail.com>
2020-10-18 01:15:34 +0600

    Model View In Listings App from backend

--- AJ_RealEstate/listings/views.py
+++ AJ_RealEstate/listings/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-
+from .models import Listing
 
 # Create your views here.
 
 def listings_index(request):
-    return render(request, 'listings/listings_index.html')
+    listing_list = Listing.objects.all()
 
+    context = {
+        'listing_list': listing_list
+    }
+    return render(request, 'listings/listings_index.html', context)
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    listing_id = Listing.objects.get(id=listing_id)
+    seller_of_the_month = Listing.objects.filter()
+    context = {
+        'listing_id': listing_id
+    }
+
+    return render(request, 'listings/listing.html', context)
