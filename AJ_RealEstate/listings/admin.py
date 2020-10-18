d4cbca41187b32beb830e6fc21accd66bc3dff69
Mohammad Sakib Hossain <sakib.swe.diu@gmail.com>
2020-10-18 20:40:42 +0600

    Admin Customization for Realtors & Listings App

--- AJ_RealEstate/listings/admin.py
+++ AJ_RealEstate/listings/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Listing
 
+
 # Register your models here.
 
-admin.site.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Listing
 
+    list_display = ['id', 'title', 'address', 'city', 'price', 'price', 'photo_main', 'realtor', 'is_published']
+    list_display_links = ['id', 'title']
+    list_filter = ['realtor']
+    list_editable = ('is_published',)
+    list_per_page = 10
+
+
+admin.site.register(Listing, ListingAdmin)
--- AJ_RealEstate/realtors/admin.py
+++ AJ_RealEstate/realtors/admin.py
@@ -3,4 +3,16 @@ from .models import Realtor
 
 # Register your models here.
 
-admin.site.register(Realtor)
+class RealtorAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Realtor
+
+    list_display = ['id', 'name', 'photo', 'email', 'phone', 'is_mvp']
+    list_display_links = ['id']
+    list_editable = ('is_mvp',)
+    list_filter = ('name',)
+    search_fields = ('name', 'email', 'phone', 'message',)
+    list_per_page = 15
+
+
+admin.site.register(Realtor, RealtorAdmin)
