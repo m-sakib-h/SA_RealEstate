ecace0dedd157f46f2978f6b50d6eb736ff7355c
Mohammad Sakib Hossain <sakib.swe.diu@gmail.com>
2020-10-18 18:31:02 +0600

    Paginator in Listings App

--- AJ_RealEstate/listings/views.py
+++ AJ_RealEstate/listings/views.py
@@ -1,15 +1,27 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from .models import Listing
 
 # Create your views here.
 
 def listings_index(request):
-    listing_list = Listing.objects.all()
+    listing_list = Listing.objects.all()  # [:1]    # Suppose 6 listing
+
+    paginator = Paginator(listing_list, 3)
+    page = request.GET.get('page', 1)
+
+    try:
+        paginator_listing_ = paginator.page(page)
+    except PageNotAnInteger:
+        paginator_listing_ = paginator.page(1)      # By_Default kon page show korba
+    except EmptyPage:
+        paginator_listing_ = paginator.num_pages
 
     context = {
-        'listing_list': listing_list
+        'listing_list': paginator_listing_
     }
     return render(request, 'listings/listings_index.html', context)
+
 
 def listing(request, listing_id):
     listing_id = Listing.objects.get(id=listing_id)
